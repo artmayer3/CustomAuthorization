d6ab72e8de970e9d669cf9a7d880a310e94e7cc6
artmayer3 <a.seliverstovqq@gmail.com>
2021-04-27 01:25:04 +0500

    Initial commit

---
+++ Lab1/AdminWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using Npgsql;
+using System;
+using System.Data;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+
+namespace Lab1
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+        private int saltLengthLimit = 7;
+        private string connectionString;
+        private DataTable usersTable;
+        private NpgsqlConnection connection;
+        private NpgsqlDataAdapter adapter;
+        private NpgsqlCommandBuilder comandbuilder;
+        public AdminWindow(string connection)
+        {
+            InitializeComponent();
+            connectionString = connection;
+            Dispatcher.BeginInvoke((Action)(() => FillTable()));
+        }
+        private void FillTable()
+        {
+            try
+            {
+                connection = new NpgsqlConnection(connectionString);
+                var sqlQuery = "SELECT * FROM users;";
+                using (NpgsqlCommand command = new NpgsqlCommand(sqlQuery, connection))
+                {
+                    adapter = new NpgsqlDataAdapter(command);
+                    usersTable = new DataTable();
+
+                    connection.Open();
+                    adapter.Fill(usersTable);
+                    comandbuilder = new NpgsqlCommandBuilder(adapter);
+                    adapter.InsertCommand = comandbuilder.GetInsertCommand();
+                    adapter.UpdateCommand = comandbuilder.GetUpdateCommand();
+                    adapter.DeleteCommand = comandbuilder.GetDeleteCommand();
+                    usersGrid.ItemsSource = usersTable.DefaultView;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private byte[] GetSalt()
+        {
+            return GetSalt(saltLengthLimit);
+        }
+        private byte[] GetSalt(int maximumSaltLength)
+        {
+            var salt = new byte[maximumSaltLength];
+            using (RNGCryptoServiceProvider random = new RNGCryptoServiceProvider())
+            {
+                random.GetNonZeroBytes(salt);
+            }
+
+            return salt;
+        }
+
+        private void AddUser_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (SHA256Managed sHA256 = new SHA256Managed())
+                {
+                    var salt = BitConverter.ToString(GetSalt()).Replace("-", string.Empty).ToLower();
+                    var password = BitConverter.ToString(sHA256.ComputeHash(Encoding.UTF8.GetBytes($"{Auth.ConvertByteToString(sHA256.ComputeHash(Encoding.UTF8.GetBytes(passwordField.Password)))}{salt}"))).Replace("-", string.Empty).ToLower();
+                    var sqlQuery = $@"INSERT INTO public.users(username, password, salt, ""isAdmin"") VALUES('{usernameField.Text}', '{password}', '{salt}', {isAdmin.IsChecked});";
+
+                    using (NpgsqlCommand command = new NpgsqlCommand(sqlQuery, connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                usernameField.Text = string.Empty;
+                passwordField.Password = string.Empty;
+                isAdmin.IsChecked = false;
+                Dispatcher.BeginInvoke((Action)(() => FillTableAfterAdd()));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void FillTableAfterAdd()
+        {
+            var sqlQuery = "SELECT * FROM users;";
+            using (NpgsqlCommand command = new NpgsqlCommand(sqlQuery, connection))
+            {
+                adapter = new NpgsqlDataAdapter(command);
+                usersTable.Clear();
+                adapter.Fill(usersTable);
+                comandbuilder = new NpgsqlCommandBuilder(adapter);
+                adapter.InsertCommand = comandbuilder.GetInsertCommand();
+                adapter.UpdateCommand = comandbuilder.GetUpdateCommand();
+                adapter.DeleteCommand = comandbuilder.GetDeleteCommand();
+                usersGrid.ItemsSource = usersTable.DefaultView;
+            }
+        }
+        private void UpdateDB() => adapter.Update(usersTable);
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e) => Dispatcher.BeginInvoke((Action)(() => UpdateDB()));
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (usersGrid.SelectedItems != null)
+            {
+                for (int i = 0; i < usersGrid.SelectedItems.Count; i++)
+                {
+                    DataRowView datarowView = usersGrid.SelectedItems[i] as DataRowView;
+                    if (datarowView != null)
+                    {
+                        DataRow dataRow = datarowView.Row;
+                        dataRow.Delete();
+                    }
+                }
+            }
+            Dispatcher.BeginInvoke((Action)(() => UpdateDB()));
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            connection.CloseAsync();
+            new Auth().Show();
+        }
+    }
+}
---
+++ Lab1/Auth.xaml.cs
@@ -0,0 +1,86 @@
+﻿using Npgsql;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+
+namespace Lab1
+{
+    /// <summary>
+    /// Логика взаимодействия для Auth.xaml
+    /// </summary>
+    public partial class Auth : Window
+    {
+        private const string connectionString = "Server=127.0.0.1;Port=5432;User Id=postgres;Password=218178artem;Database=db_Lab_DGIB;";
+        public Auth()
+        {
+            InitializeComponent();
+            //using (SHA256Managed sHA256 = new SHA256Managed())
+            //    BitConverter.ToString(sHA256.ComputeHash(Encoding.UTF8.GetBytes($"{ConvertByteToString(sHA256.ComputeHash(Encoding.UTF8.GetBytes("123")))}fec5d4fc2f5cf5"))).Replace("-", string.Empty).ToLower();
+        }
+
+        private void OpenAdminWindow()
+        {
+            AdminWindow mainWindow = new AdminWindow(connectionString);
+            mainWindow.Show();
+            Dispatcher.BeginInvoke((Action)(() => Close()));
+        }
+        private void OpenUserWindow()
+        {
+            UserWindow userWindow = new UserWindow();
+            userWindow.Show();
+            Dispatcher.BeginInvoke((Action)(() => Close()));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            labelError.Content = string.Empty;
+
+            var sqlQuery = $"SELECT * FROM users WHERE username='{loginBox.Text}';";
+
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (NpgsqlCommand command = new NpgsqlCommand(sqlQuery, connection))
+                    {
+                        using (NpgsqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                var password = reader.GetString(2);
+                                var salt = reader.GetString(3);
+
+                                using (SHA256Managed sHA256 = new SHA256Managed())
+                                {
+                                    if (password.Equals(BitConverter.ToString(sHA256.ComputeHash(Encoding.UTF8.GetBytes($"{ConvertByteToString(sHA256.ComputeHash(Encoding.UTF8.GetBytes(passwordBox.Password)))}{salt}"))).Replace("-", string.Empty).ToLower()))
+                                    {
+                                        if (reader.GetBoolean(4))
+                                        {
+                                            Dispatcher.BeginInvoke((Action)(() => OpenAdminWindow()));
+                                        }
+                                        else
+                                        {
+                                            Dispatcher.BeginInvoke((Action)(() => OpenUserWindow()));
+                                        }
+                                        return;
+                                    }
+                                }
+                            }
+                            labelError.Content = "Неверное имя пользователя или пароль";
+                            return;
+                        }
+                    }
+                }
+            }
+            catch (NpgsqlException)
+            {
+                labelError.Content = "Неверное имя пользователя или пароль";
+                return;
+            }
+        }
+        public static string ConvertByteToString(byte[] source) => source != null ? Encoding.UTF8.GetString(source) : null;
+        private void Button_Click_1(object sender, RoutedEventArgs e) => Close();
+    }
+}
